0ba4ff317d3bd8bac750991d32bf20441b7de285
Philipp Emanuel Weidmann <pew@worldwidemann.com>
2021-12-09 10:05:30 +0530

    Save REPL input history

--- savage/src/main.rs
+++ savage/src/main.rs
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (C) 2021  Philipp Emanuel Weidmann <pew@worldwidemann.com>
 
-use std::collections::HashMap;
+use std::{collections::HashMap, fs};
 
+use directories::ProjectDirs;
 use rustyline::{error::ReadlineError, Editor};
 use savage_core::expression::Expression;
 
 fn main() {
+    let history_path = ProjectDirs::from("com.worldwidemann", "", "Savage")
+        .expect("unable to locate data directory")
+        .data_dir()
+        .join("history");
+
     let mut editor = Editor::<()>::new();
+
+    editor.load_history(&history_path).ok();
 
     loop {
         match editor.readline("> ") {
@@ -37,4 +45,15 @@ fn main() {
             }
         }
     }
+
+    fs::create_dir_all(
+        history_path
+            .parent()
+            .expect("unable to determine parent directory of history file"),
+    )
+    .expect("unable to create data directory");
+
+    editor
+        .save_history(&history_path)
+        .expect("unable to save input history");
 }
