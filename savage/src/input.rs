b36adb5e0c6128579cbdd770bf7fbaa76cf64601
Philipp Emanuel Weidmann <pew@worldwidemann.com>
2021-12-18 11:23:54 +0530

    Highlight REPL prompt and input

--- savage/src/input.rs
+++ savage/src/input.rs
@@ -1,15 +1,125 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (C) 2021  Philipp Emanuel Weidmann <pew@worldwidemann.com>
 
+use std::borrow::Cow;
+
+use ansi_term::Style;
+use lazy_static::lazy_static;
+use regex::Regex;
 use rustyline::{
+    highlight::Highlighter,
     validate::{ValidationContext, ValidationResult, Validator},
     Result,
 };
-use rustyline_derive::{Completer, Helper, Highlighter, Hinter};
+use rustyline_derive::{Completer, Helper, Hinter};
 use savage_core::{expression::Expression, parse::ErrorReason};
 
-#[derive(Completer, Helper, Highlighter, Hinter)]
+enum TokenType {
+    Literal,
+    Variable,
+    Operator,
+    Bracket,
+    Separator,
+    Whitespace,
+    Invalid,
+}
+
+fn tokenize(input: &str) -> Vec<(String, TokenType)> {
+    use TokenType::*;
+
+    lazy_static! {
+        static ref REGEX: Regex = Regex::new(
+            &[
+                r"(?P<literal>[0-9]+(?:\.[0-9]+)?|true|false)",
+                r"(?P<variable>[a-zA-Z_][a-zA-Z0-9_]*)",
+                r"(?P<operator>[+\-*/%^!=<>&|]+)",
+                r"(?P<bracket>[()\[\]])",
+                r"(?P<separator>,)",
+                r"(?P<whitespace>\s+)",
+            ]
+            .join("|"),
+        )
+        .unwrap();
+    }
+
+    let mut tokens = Vec::new();
+
+    let mut last_token_end = 0;
+
+    for captures in REGEX.captures_iter(input) {
+        let token_type = if captures.name("literal").is_some() {
+            Literal
+        } else if captures.name("variable").is_some() {
+            Variable
+        } else if captures.name("operator").is_some() {
+            Operator
+        } else if captures.name("bracket").is_some() {
+            Bracket
+        } else if captures.name("separator").is_some() {
+            Separator
+        } else if captures.name("whitespace").is_some() {
+            Whitespace
+        } else {
+            unreachable!()
+        };
+
+        let token_range = captures.get(0).unwrap().range();
+
+        if last_token_end < token_range.start {
+            tokens.push((input[last_token_end..token_range.start].to_owned(), Invalid));
+        }
+
+        last_token_end = token_range.end;
+
+        tokens.push((input[token_range].to_owned(), token_type));
+    }
+
+    if last_token_end < input.len() {
+        tokens.push((input[last_token_end..].to_owned(), Invalid));
+    }
+
+    tokens
+}
+
+#[derive(Completer, Helper, Hinter)]
 pub struct InputHelper {}
+
+impl Highlighter for InputHelper {
+    fn highlight_prompt<'b, 's: 'b, 'p: 'b>(
+        &'s self,
+        prompt: &'p str,
+        _default: bool,
+    ) -> Cow<'b, str> {
+        Cow::Owned(Style::new().bold().paint(prompt).to_string())
+    }
+
+    fn highlight<'l>(&self, line: &'l str, _pos: usize) -> Cow<'l, str> {
+        use ansi_term::Colour::*;
+        use TokenType::*;
+
+        let mut highlighted_line = String::new();
+
+        for (token, token_type) in tokenize(line) {
+            let style = match token_type {
+                Literal => Cyan.into(),
+                Variable => Green.into(),
+                Operator => Purple.into(),
+                Bracket => Style::new(),
+                Separator => Style::new(),
+                Whitespace => Style::new(),
+                Invalid => Red.into(),
+            };
+
+            highlighted_line.push_str(&style.paint(token).to_string());
+        }
+
+        Cow::Owned(highlighted_line)
+    }
+
+    fn highlight_char(&self, _line: &str, _pos: usize) -> bool {
+        true
+    }
+}
 
 impl Validator for InputHelper {
     fn validate(&self, ctx: &mut ValidationContext) -> Result<ValidationResult> {
