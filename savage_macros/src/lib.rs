6d75fd70dd89de9f48ba58025a8247f3ae138679
Philipp Emanuel Weidmann <pew@worldwidemann.com>
2022-02-14 09:49:14 +0530

    Update copyright year

--- savage_macros/src/lib.rs
+++ savage_macros/src/lib.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-// Copyright (C) 2021  Philipp Emanuel Weidmann <pew@worldwidemann.com>
+// Copyright (C) 2021-2022  Philipp Emanuel Weidmann <pew@worldwidemann.com>
 
 use darling::FromMeta;
 use proc_macro::TokenStream;
